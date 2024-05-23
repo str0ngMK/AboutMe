cf318d2dde3bc492845f3c060257b34a606eb1af
str0ngmk <str0ngmk@diquest.com>
2024-05-23 19:24:09 +0900

    전면 개편

--- src/main/java/com/about/me/controller/BoardController.java
+++ src/main/java/com/about/me/controller/BoardController.java
@@ -1,13 +1,7 @@
 package com.about.me.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -44,55 +39,18 @@ public class BoardController {
 		return ResponseEntity.ok().body(result);
 	}
 	
-	private final String uploadDir = Paths.get("C:", "Users", "diquest", "Desktop", "img").toString();
-	
 	@PostMapping("/image-upload")
-	public ResponseEntity<Map<String, Object>> uploadEditorImage(@RequestParam(name = "image") MultipartFile image) {
-		Map<String, Object> result = new HashMap<>();
-		if (image.isEmpty()) {
-			result.put("result", "");
-			return ResponseEntity.ok().body(result);
-		}
-		String orgFilename = image.getOriginalFilename();
-		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-		String extension = orgFilename.substring(orgFilename.lastIndexOf(".") + 1);
-		String saveFilename = uuid + "." + extension;
-		String fileFullPath = Paths.get(uploadDir, saveFilename).toString();
-		
-		File dir = new File(uploadDir);
-		if (dir.exists() == false) {
-			dir.mkdirs();
-		}
-		
-		try {
-			File uploadFile = new File(fileFullPath);
-			image.transferTo(uploadFile);
-			result.put("result", saveFilename);
-			return ResponseEntity.ok().body(result);
-		} catch(IOException e) {
-			throw new RuntimeException(e);
-		}
+	public ResponseEntity<String> uploadEditorImage(@RequestParam(name = "image") MultipartFile image) {
+		return ResponseEntity.ok().body(boardService.uploadImage(image));
 	}
 	
 	@GetMapping("/image-print")
     public ResponseEntity<byte[]> printEditorImage(@RequestParam(name = "filename") String filename) {
-        // 업로드된 파일의 전체 경로
-        String fileFullPath = Paths.get(uploadDir, filename).toString();
-
-        // 파일이 없는 경우 예외 throw
-        File uploadedFile = new File(fileFullPath);
-        if (uploadedFile.exists() == false) {
-            throw new RuntimeException();
-        }
-
-        try {
-            // 이미지 파일을 byte[]로 변환 후 반환
-            byte[] imageBytes = Files.readAllBytes(uploadedFile.toPath());
-            return ResponseEntity.ok().body(imageBytes);
-
-        } catch (IOException e) {
-            // 예외 처리는 따로 해주는 게 좋습니다.
-            throw new RuntimeException(e);
-        }
+    	return ResponseEntity.ok().body(boardService.printImage(filename));
+    }
+    
+    @PostMapping("/save")
+    public ResponseEntity<?> saveBoard(@RequestBody Map<String, Object> body){
+    	return null;
     }
 }
--- src/main/java/com/about/me/service/BoardService.java
+++ src/main/java/com/about/me/service/BoardService.java
@@ -1,12 +1,20 @@
 package com.about.me.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.about.me.config.ApplicationConfig;
 import com.about.me.dto.BoardDto;
 import com.about.me.entity.BoardEntity;
 import com.about.me.repository.BoardRepository;
@@ -16,6 +24,9 @@ public class BoardService {
 	
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ApplicationConfig applicationConfig;
 	
 	public List<BoardDto> getBoardList() {
 		List<BoardEntity> list = boardRepository.findByDelYn(false);
@@ -34,6 +45,7 @@ public class BoardService {
 		return result;
 	}
 	
+	
 	public BoardDto getBoard(long no) {
 		Optional<BoardEntity> result = boardRepository.findByNo(no);
 		BoardDto dto = new BoardDto();
@@ -50,6 +62,64 @@ public class BoardService {
 			return null;
 		}
 		return dto;
+	}
+	
+	public String uploadImage(MultipartFile image){
+		String uploadDir = applicationConfig.getImgRepoPath();
+		
+		if (image.isEmpty()) {
+			return "";
+		}
+		
+		String orgFilename = image.getOriginalFilename();
+		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+		String extension = orgFilename.substring(orgFilename.lastIndexOf(".") + 1);
+		String saveFilename = uuid + "." + extension;
+		String fileFullPath = Paths.get(uploadDir, saveFilename).toString();
+		
+		File dir = new File(uploadDir);
+		if (dir.exists() == false) {
+			throw new RuntimeException();
+		}
+		
+		try {
+			File uploadFile = new File(fileFullPath);
+			image.transferTo(uploadFile);
+			return saveFilename;
+		} catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public byte[] printImage(String filename) {
+		String uploadDir = applicationConfig.getImgRepoPath();
+		// 업로드된 파일의 전체 경로
+        String fileFullPath = Paths.get(uploadDir, filename).toString();
+
+        // 파일이 없는 경우 예외 throw
+        File uploadedFile = new File(fileFullPath);
+        if (uploadedFile.exists() == false) {
+            throw new RuntimeException();
+        }
+
+        try {
+            // 이미지 파일을 byte[]로 변환 후 반환
+            byte[] imageBytes = Files.readAllBytes(uploadedFile.toPath());
+            return imageBytes;
+
+        } catch (IOException e) {
+            // 예외 처리는 따로 해주는 게 좋습니다.
+            throw new RuntimeException(e);
+        }
+	}
+	
+	public void saveBoard(Map<String, Object> body) {
+		String title = String.valueOf(body.get("title"));
+		String content = String.valueOf(body.get("content"));
+		
+		
+		
+		
 	}
 
 }
